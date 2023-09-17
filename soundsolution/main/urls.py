90bf0e1fd39f289725fd35e7b0fae033f670bc0b
Dest1ni <vilo76671@gmail.com>
2023-09-17 13:15:48 +0300

    iniit

--- soundsolution/main/urls.py
+++ soundsolution/main/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'main'
 
 urlpatterns = [
-    path('main-page/',views.MainPage.as_view(),name='main-page'),
+    path('',views.MainPage.as_view(),name='main-page'),
     path('contacts/',views.ContactPage.as_view(),name='contacts'),
     path('about/',views.AboutPage.as_view(),name='about'),
 ]
